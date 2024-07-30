e11a606f35fb51c2e82e03a80f447d1f560397e8
Kaushal Sindhu <kaushal1308sindhu@gmail.com>
2024-07-30 20:40:04 +0530

    config commit

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   server: {
-    proxy:{
-      '/api': 'https://opp-backend.onrender.com',
-    }
+    '/api': {
+        target: 'https://opp-backend.onrender.com', // Your backend URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api prefix
+      },
   },
   plugins: [react()],
 })
