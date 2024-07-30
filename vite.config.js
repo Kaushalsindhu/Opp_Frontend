263096b7480c8731922691921a498ef7b5a4ce62
Kaushal Sindhu <kaushal1308sindhu@gmail.com>
2024-07-30 15:11:26 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy:{
-      '/api': 'https://opp-backend.onrender.com/',
+      '/api': 'https://opp-backend.onrender.com',
     }
   },
   plugins: [react()],
